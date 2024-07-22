ea3bd2e01babd95edba5db1f3bc34857f5b9d07f
Khumoyun003 <alimovkh0303@gmail.com>
2024-07-22 15:29:34 +0500

    Yangi fayl qo'shtim

---
+++ temp.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+Это временный скриптовый файл.
+"""
+# kun = 'yakshanba'
+# if input("Bugun qanday kun ?") == kun.lower():
+#     print("Bugun dam olish kuni")
+# else:
+#     print("Bugun ish kuni ekan !")
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car!='gm':
+#         print(car.title())
+#     else:
+#         print(car.upper())
+# login = input("Ismingiznni kiriting >>>  ")
+# if login.lower()== "admin":
+#     print("Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi?")
+# else:
+#     print(f"Xush kelibsiz {login.title()}!")
+    
+a = int(input("Birinchi sonni kiriting >>>  "))
+b = int(input("Ikkinchi sonni kiriting >>>  "))
+if a == b:
+    print(f"Ikki son ham bir-biriga teng {a} = {b}")
+else:
+    c = input("Bu sonlarni bir biriga qo'shmoxchimisiz ?")   
+    if c.lower() == 'xa':
+        print(f"{a} + {b} teng {a+b} ga")    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
